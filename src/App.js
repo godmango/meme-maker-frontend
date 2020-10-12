9a019397796de40c593a29744054cfd3406c7e9a
Jun Young Cho <godguman@gmail.com>
2020-10-12 16:23:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+// import "App.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import { Container } from "react-bootstrap";
+// import Button from "components/Button";
+
+// function App() {
+// 	return (
+// 		<div className="text-center">
+// 			<Container>
+// 				<h1>My new React App</h1>
+// 			</Container>
+// 		</div>
+// 	);
+// }
+
+// export default App;
+
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import Routes from "./containers/Routes";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faImage, faPlusSquare } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faImage, faPlusSquare);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<Routes />
+		</Router>
+	);
 }
 
 export default App;
